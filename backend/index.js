e47b2aa2b53987f80b5bfae8d2e64eee2d769dca
Or Giny <orginy@Ors-MacBook-Pro.local>
2023-11-23 15:21:45 +0200

    final

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;;
 
 const juiceFlavors = ['Lemonade🍋', 'Orange🍊', 'Grape🍇', 'Strawberry🍓 & Banana🍌', 'Mango🥭', 'Nectarine😥','Peach🍑','Pineapple🍍','Blueberry🫐','Tomato🍅','Tropical🍹','Pear🍐','תפוח אגס סברס🎨','Watermelon🍉'];
 
